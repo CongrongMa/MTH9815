bfcf0650cd00c12ca41dee86ee8c7c920f34e143
CongrongMa <106537004+CongrongMa@users.noreply.github.com>
2022-11-22 19:05:40 -0500

    Add files via upload
    fix a typo

--- quiz/main.cpp
+++ quiz/main.cpp
@@ -7,16 +7,20 @@ using namespace std;
 int main(int argc, char **argv)
 //int main()
 {
-    cout<<'Test quicksort:'<<endl;
+    cout<<"Test quicksort for:"<<endl;
     vector<int> a={8,3,7,13,1,9};
+    for(auto item:a){
+        cout<<item<<",";
+    }
+    cout<<endl;
     quickSort qs;
     qs(a);
     for(auto item:a){
         cout<<item<<",";
     }
     cout<<endl;
-    
-    cout<<'Test max heap'<<endl;
+
+    cout<<"Test max heap"<<endl;
     maxHeap heap;
     heap.Add(8);
     heap.Add(3);
@@ -25,16 +29,16 @@ int main(int argc, char **argv)
     heap.Add(1);
     heap.Add(9);
     heap.printHeap();
-    cout<<'remove'<<endl;
+    cout<<"remove"<<endl;
     heap.Remove();
     heap.printHeap();
-    cout<<'remove'<<endl;
+    cout<<"remove"<<endl;
     heap.Remove();
     heap.printHeap();
-    cout<<'remove index: 2'<<endl;
+    cout<<"remove index: 2"<<endl;
     heap.Remove(2);
     heap.printHeap();
-    cout<<'remove index 1'<<endl;
+    cout<<"remove index 1"<<endl;
     heap.Remove(1);
     heap.printHeap();
 
